b8a6cfbba7824042d06e0c129517b721c3aa4846
Open-Debin <michaeldbmeng19@outlook.com>
2021-04-08 06:04:03 +0100

    update

--- metadataset/lib/paths.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-from pathlib import Path
-
-PROJECT_ROOT = str((Path(__file__).parent / '..').resolve())
-META_DATASET_ROOT = "../../code_others/meta-dataset" 
-META_RECORDS_ROOT = "../../code_others/meta-dataset/dataset_records" 
-META_DATA_ROOT = '/'.join(META_RECORDS_ROOT.split('/')[:-1])
